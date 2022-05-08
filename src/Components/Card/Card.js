316345bfc928084cd2a184ad9d492aba0af0944a
Torikul <toriqulislam142@gmail.com>
2022-05-08 12:05:04 -0500

    signUp form designed compeleted

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Card.css'
-const Card = ({food}) => {
-    const {name,image,title,price} = food
+const Card = ({ food }) => {
+    const { name, image, title, price } = food
     return (
         <div className="item-container">
-             <div className='single-item'>
-            <img src={image} alt="" />
-            <h2>{name}</h2>
-            <span>{title}</span>
-            <h2> $ { price}</h2>
+            <div className='single-item'>
+                <img src={image} alt="" />
+                <h2>{name}</h2>
+                <span>{title}</span>
+                <h2> $ {price}</h2>
+            </div>
         </div>
-       </div>
     );
 };
 
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './SignUp.css'
+import Logo from '../../../src/images/logo2.png'
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>this is signup page </h1>
+        <div className='form-container'>
+
+            <form className='signUp-form' >
+                <img className='form-logo' src={Logo} alt="logo" />
+                <input type="text" placeholder='Name' />
+                <input type="text" placeholder='Email' />
+                <input type="text" placeholder='Password'/>
+                <input type="text" placeholder='Confirm password' />
+                <button className='signUp-form-button' type='submit'>Sign Up</button>
+                <p>Already have an account ?</p>
+            </form>
+
+
+
         </div>
     );
 };
